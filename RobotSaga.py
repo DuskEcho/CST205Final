92967ad4707442a74d36448ef717e24dea97de41
Greg <gbrown2788@gmail.com>
2019-04-22 18:00:18 -0700

    Fixed another issue in loadBot (hp related)

--- RobotSaga.py
+++ RobotSaga.py
@@ -3706,6 +3706,7 @@ def loadBot():
         WorldData.bot1.level = int(line[len("Level:"):line.index('\n')])
       elif "MaxHp:" in line:
         WorldData.bot1.maxHp = int(line[len("MaxHp:"):line.index('\n')])
+        WorldData.bot1.changeHp(WorldData.bot1.maxHp)
       elif "CurrentHp:" in line:
         WorldData.bot1.maxHp = int(line[len("CurrentHp:"):line.index('\n')])
       elif "Xp:" in line:
